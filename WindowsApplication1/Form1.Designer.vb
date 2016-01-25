b6bb54112c7f36c0082f32a8a2b0a45bae332cef
sguha-work <sguha1988.life@gmail.com>
2016-01-25 12:32:20 +0530

    initial commit

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.btn_loadFusionCharts = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(133, 55)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(325, 174)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'btn_loadFusionCharts
+        '
+        Me.btn_loadFusionCharts.Location = New System.Drawing.Point(210, 261)
+        Me.btn_loadFusionCharts.Name = "btn_loadFusionCharts"
+        Me.btn_loadFusionCharts.Size = New System.Drawing.Size(167, 35)
+        Me.btn_loadFusionCharts.TabIndex = 1
+        Me.btn_loadFusionCharts.Text = "Click to load FusionCharts"
+        Me.btn_loadFusionCharts.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(568, 338)
+        Me.Controls.Add(Me.btn_loadFusionCharts)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents btn_loadFusionCharts As System.Windows.Forms.Button
+
+End Class
